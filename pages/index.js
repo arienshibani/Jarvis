94821e46ea516b6a1ace7b1caba4e36201276ec1
Arien Shibani <aaari94@gmail.com>
2023-06-09 16:53:31 +0200

    feat: hahaha

--- pages/index.js
+++ pages/index.js
@@ -34,21 +34,23 @@ export default function Home() {
   if(state === "init"){
     return (
       <>
+      
         <ButtonAppBar className='appbar'/>
   
         <main style={{display: "flex", alignItems: "center", flexDirection: "column" }}>
         <div style={{paddingTop: "5rem"}}>
-  
-        <Typography variant="h3" component="h3">
-          Your professional process optimizer
+
+        <Typography paragraph="true" component="h3" style={{margin: "10em", textAlign: "center"}}>
+          Jarvis is using <b>state of the art AI tech</b>, to analyze your business processes in order to generate improvements suggestions! <br/> 
+          Using real time data from your monitoring logs, and AI trained on best practice benchmarks and blockchain.
         </Typography>
   
-  
-        <img style={{maxWidth: "100%"}} src="https://i.imgur.com/fG8C1V6.png" />
+        <img style={{maxWidth: "75%", alignItems: "center", display: "flex", margin: "auto"}} src="https://i.imgur.com/fG8C1V6.png" />
+        
         </div>
-        <Button onClick={() => handleLoading()} style={{margin: "5rem"}} variant="contained">Analyze Process</Button>
+         <Button onClick={() => handleLoading()} style={{margin: "5rem"}} variant="contained">Analyze Process</Button>
         <div>
-      
+
   
         </div>
       
@@ -72,7 +74,15 @@ export default function Home() {
   else if (state === "results") {
     return(
       <>
-    <SimpleAccordion/>
+        <Typography variant="h4" style={{margin: "10rem"}} component="h3">
+           Jarvis has found <b>2</b> areas of your process that needs attention. 
+        </Typography>
+        <img style={{maxWidth: "50%", alignItems: "center", display: "flex", margin: "auto"}} src="https://i.imgur.com/fG8C1V6.png" />
+
+      <div style={{maxWidth: "50%", alignItems: "center", display: "flex", margin: "auto", marginTop: "3rem"}}>
+
+        <SimpleAccordion/>
+      </div>
       </>
       
     )

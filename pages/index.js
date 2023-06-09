6ca5f11720c796071f5753bc64ee4db2519bb251
Arien Shibani <aaari94@gmail.com>
2023-06-09 16:56:38 +0200

    things

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,95 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import ButtonAppBar from './components/AppBar';
-import BasicTable from './components/Table';
-import Button from '@mui/material/Button';
+import { Typography, Button, CircularProgress } from '@mui/material';
+import Card from '@mui/material/Card';
+import SimpleAccordion from './components/Accordion';
+import { display } from '@mui/system';
+import BasicSelect from './components/Select';
+import React, { useState, useEffect } from 'react';
+
+
 
 
 export default function Home() {
-  return (
-    <>
-    <ButtonAppBar className='appbar'/>
-    
-    
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          background-color: pink;
-        }
-        appbar {
-          background-color: #cf4136;
-          width: 100%;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
+  const [state, setState] = useState("init");
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-  </>
-  )
+  const handleLoading = () => {
+    setState("loading")
+      
+    setTimeout(() => {
+      setState("results");
+
+      console.log("Value changed to", state);
+    }, 5000);
+  
+
+    console.log("det funke")
+  };
+  
+
+  useEffect(() => {
+    console.log("Value changed to", state);
+  }, [state]);
+
+
+  if(state === "init"){
+    return (
+      <>
+      
+        <ButtonAppBar className='appbar'/>
+  
+        <main style={{display: "flex", alignItems: "center", flexDirection: "column" }}>
+        <div style={{paddingTop: "5rem"}}>
+
+        <Typography paragraph="true" component="h3" style={{margin: "10em", textAlign: "center"}}>
+          Jarvis is using <b>state of the art AI tech</b>, to analyze your business processes in order to generate improvements suggestions! <br/> 
+          Using real time data from your monitoring logs, and AI trained on best practice benchmarks and blockchain.
+        </Typography>
+  
+        <img style={{maxWidth: "75%", alignItems: "center", display: "flex", margin: "auto"}} src="https://i.imgur.com/fG8C1V6.png" />
+        
+        </div>
+         <Button onClick={() => handleLoading()} style={{margin: "5rem"}} variant="contained">Analyze Process</Button>
+        <div>
+
+  
+        </div>
+      
+        </main>
+      </>)
+  } else if (state === "loading") {
+    return(
+  
+      <main style={{display: "flex", alignItems: "center", flexDirection: "column" }}>
+      
+        <Typography variant="h4" style={{margin: "10rem"}} component="h3">
+        Doing super advanced AI analysis...
+        </Typography>
+
+        <CircularProgress />
+  
+      
+      </main>
+    )
+  }
+  else if (state === "results") {
+    return(
+      <>
+        <Typography variant="h4" style={{margin: "10rem"}} component="h3">
+           Jarvis has found <b>2</b> areas of your process that needs attention. 
+        </Typography>
+        <img style={{maxWidth: "50%", alignItems: "center", display: "flex", margin: "auto"}} src="https://i.imgur.com/fG8C1V6.png" />
+
+      <div style={{maxWidth: "50%", alignItems: "center", display: "flex", margin: "auto", marginTop: "3rem"}}>
+
+        <SimpleAccordion/>
+      </div>
+      </>
+      
+    )
+  }
+  else {
+    return(
+     <p>BEANS</p>
+    )
+  }
 }
